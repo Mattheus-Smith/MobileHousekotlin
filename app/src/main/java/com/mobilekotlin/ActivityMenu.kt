7958d9e1722fa4cc47d892f3b16e3aa709e478ad
SmithFernandes <smith.fernandes1337@gmail.com>
2023-01-12 08:58:23 -0400

    teste de main
    criação da ActivityMenu
    
    as outras brachs estavam com erro
    
    atualizar brach main

---
+++ app/src/main/java/com/mobilekotlin/ActivityMenu.kt
@@ -0,0 +1,11 @@
+package com.mobilekotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
--- app/src/main/java/com/mobilekotlin/MainActivity.kt
+++ app/src/main/java/com/mobilekotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mobilekotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        next()
+    }
+
+    fun next(){
+        val intent = Intent(this, ActivityMenu::class.java)
+        startActivity(intent)
     }
 }
